a4b80194d0cd5ad6b5ffdd21625c19323eb9d0f4
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2024-12-30 14:26:07 +0300

    feat: implement 'Vigenere cipher' task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    return this.#transformMessage(
+      message.toUpperCase(),
+      key.toUpperCase(),
+      true
+    );
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    return this.#transformMessage(
+      message.toUpperCase(),
+      key.toUpperCase(),
+      false
+    );
+  }
+
+  #transformMessage(message, key, action) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let skippedChars = 0;
+
+    let transformedMessage = message.split('').map((char, index) => {
+      const charIndex = alphabet.indexOf(char);
+
+      if (charIndex === -1) {
+        skippedChars++;
+        return char;
+      }
+
+      const keyIndex = alphabet.indexOf(
+        key[(index - skippedChars) % key.length]
+      );
+
+      return alphabet[
+        (charIndex + keyIndex * (action ? 1 : -1) + alphabet.length) %
+          alphabet.length
+      ];
+    });
+
+    if (!this.isDirect) transformedMessage = transformedMessage.reverse();
+
+    return transformedMessage.join('');
   }
 }
 
